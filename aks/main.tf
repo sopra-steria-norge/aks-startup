eab269f002dd6af407fa9b0a4b011e7a2a98bddc
Geir <jerstad_geir_ivar@hotmail.com>
2021-09-29 14:59:52 +0200

    aks

--- aks/main.tf
+++ aks/main.tf
@@ -19,10 +19,19 @@ data "azurerm_resource_group" "main" {
   name     = "aks-startup-d-rg-aks-infrastructure"
 }
 
-resource "azurerm_container_registry" "main" {
-  name                = "${var.AKS_STARTUP_ACR_NAME}"
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "aks-startup"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  sku                 = "Standard"
-  admin_enabled       = false
+  dns_prefix          = "aks-startup"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
--- aks/outputs.tf
+++ aks/outputs.tf
@@ -1,3 +1,9 @@
-output "azurerm_container_registry_name" {
-  value = azurerm_container_registry.main.name
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.main.kube_config.0.client_certificate
 }
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.main.kube_config_raw
+}
+
