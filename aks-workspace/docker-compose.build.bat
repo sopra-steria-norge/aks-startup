0dfa2cebf009edfbb136e12bf7cf80d603f8a00d
Geir Ivar Jerstad <geir.ivar.jerstad@opf.no>
2021-10-13 15:22:59 +0200

    docker-compose.build

--- aks-workspace/docker-compose.build.bat
+++ aks-workspace/docker-compose.build.bat
@@ -1,14 +1,4 @@
 cd %~dp0
 
-echo GITHUB_TOKEN: %GITHUB_TOKEN%
-
-COPY "%USERPROFILE%\.kube\config" "%~dp0\scripts\secrets\kubeconfig"
-
-docker rm -f aksstartup-workspace-1
-
-docker-compose -f docker-compose.yml down --remove-orphans
-
-docker network create -d overlay --attachable aksstartup_common_network
-
 @REM docker-compose -f docker-compose.yml build --progress plain --no-cache aksstartup-workspace-1
 docker-compose -f docker-compose.yml build --progress plain aksstartup-workspace-1
