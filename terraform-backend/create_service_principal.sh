201adfef413ec9a777f7d334f4ffbed962c6ba5b
Geir Ivar Jerstad <geir.ivar.jerstad@opf.no>
2021-10-13 09:22:13 +0000

    create_service_principal

--- terraform-backend/create_service_principal.sh
+++ terraform-backend/create_service_principal.sh
@@ -30,9 +30,11 @@ az ad sp create-for-rbac --name aks_startup_sp
 az role assignment create --assignee <client_id> --role Owner
 > az role assignment create --assignee 6cd93b1e-14d2-417a-abae-6b7c23173b92 --role Owner
 
+echo $ARM_CLIENT_ID
 
 az login --service-principal --username <client_id> --tenant <tenant_id>  --password PASSWORD
 > az login --service-principal --username 6cd93b1e-14d2-417a-abae-6b7c23173b92 --tenant 94ce5a37-6c1e-4cce-aaeb-3f20b96ab1af  --password myverysecretsecret
+> az login --service-principal --username $ARM_CLIENT_ID --tenant $ARM_TENANT_ID --password $ARM_CLIENT_SECRET
 
 # https://docs.microsoft.com/en-us/azure/storage/blobs/storage-blob-user-delegation-sas-create-cli
 
@@ -43,11 +45,8 @@ az role assignment create \
 
 > az role assignment create \
     --role "Storage Blob Data Contributor" \
-    --assignee 6cd93b1e-14d2-417a-abae-6b7c23173b92 \
-    --scope "/subscriptions/<subscription>/resourceGroups/aks-startup-rg/providers/Microsoft.Storage/storageAccounts/aksstartupsttfstate"
-
-
-
+    --assignee $ARM_CLIENT_ID \
+    --scope "/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/aks-startup-rg/providers/Microsoft.Storage/storageAccounts/aksstartupsttfstate"
 
 az role assignment list --assignee <tenant_id>
 
