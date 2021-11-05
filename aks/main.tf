5ddaccbbeba69830a6906f96a071974bce433706
geirivarjerstad <geir.jerstad@soprasteria.com>
2021-11-05 14:47:19 +0000

    node_count = 1

--- aks/main.tf
+++ aks/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_kubernetes_cluster" "main" {
 
   default_node_pool {
     name       = "default"
-    node_count = 2
+    node_count = 1
     vm_size    = "Standard_D2_v3"
   }
 
